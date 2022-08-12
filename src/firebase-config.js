322c144169724a957067307d85eda2ff2da872ec
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2022-08-11 21:45:32 -0400

    update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDefJwkSz1ZhK8TmfB1852MGIdvzyXm_ec",
-    authDomain: "messaging-api-bd2f1.firebaseapp.com",
-    projectId: "messaging-api-bd2f1",
-    storageBucket: "messaging-api-bd2f1.appspot.com",
-    messagingSenderId: "942477271357",
-    appId: "1:942477271357:web:b481e790514361cf1ce554",
-    measurementId: "G-VNLS23GCKM"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
